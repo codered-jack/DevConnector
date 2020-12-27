42bb63ca4951ec3b5e4ed67913b486cc41a29eee
Sumit Kumar <sumit.kumar@pinelabs.com>
2020-12-27 22:17:35 +0530

    user login token generation

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,7 +2,9 @@ const express = require('express');
 
 const router = express.Router();
 const gravatar = require('gravatar');
-const bcrypt = require('bcryptjs')
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const config = require('config');
 const { check, validationResult } = require('express-validator');
 
 const User = require('../../models/User');
@@ -34,24 +36,41 @@ router.post(
       let user = await User.findOne({ email });
 
       if (user) {
-        return res.status(400).json({ errors: [{ msg: 'User already exist' }] });
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'User already exist' }] });
       }
 
-      const avatar = gravatar.url(email,{
-          s:'200',
-          r:'pg',
-          d:'mm'
-      })
+      const avatar = gravatar.url(email, {
+        s: '200',
+        r: 'pg',
+        d: 'mm',
+      });
 
-      user = new User({name,email,avatar,password})
+      user = new User({ name, email, avatar, password });
 
-      const salt = await bcrypt.genSalt(10)
+      const salt = await bcrypt.genSalt(10);
 
-      user.password = await bcrypt.hash(password,salt)
+      user.password = await bcrypt.hash(password, salt);
 
-      await user.save()
+      await user.save();
 
-      res.send('User registered');
+      const payload = {
+        user: {
+          id: user.id,
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+
+          res.json({ token });
+        }
+      );
     } catch (err) {
       console.error(err.message);
       res.status(500).send('Server error');
