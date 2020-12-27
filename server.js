440df6a38dfa43b754082a8a5dc1c7cf734d2b2f
Sumit Kumar <sumit.kumar@pinelabs.com>
2020-12-27 21:32:12 +0530

    register user

--- server.js
+++ server.js
@@ -1,10 +1,23 @@
-const express = require("express")
-
+const express = require('express');
+const connectDB = require('./config/db');
 const app = express();
 
-const PORT = process.env.PORT || 5000
+const userRouter = require('./routes/api/users');
+const authRouter = require('./routes/api/auth');
+const profileRouter = require('./routes/api/profile');
+const postsRouter = require('./routes/api/posts');
 
+connectDB();
 
-app.listen(PORT,()=>{
-        console.log(`Server started on PORT ${PORT}`)
-})
+app.use(express.json({ extended: false }));
+
+app.use('/api/users', userRouter);
+app.use('/api/auth', authRouter);
+app.use('/api/profile', profileRouter);
+app.use('/api/posts', postsRouter);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(`Server started on PORT ${PORT}`);
+});
